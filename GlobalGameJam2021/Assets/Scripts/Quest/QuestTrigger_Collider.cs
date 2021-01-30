45265a34e73dbf6dc4f4ed961ea868d556f9e202
vprime <myself@vincentprime.com>
2021-01-29 22:00:43 -0700

    Adding Collider and Proximity  + Use key triggers

---
+++ GlobalGameJam2021/Assets/Scripts/Quest/QuestTrigger_Collider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger_Collider : QuestTrigger
+{
+    [SerializeField] private string triggerTag = "player";
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag(triggerTag)) 
+            Trigger();
+    }
+}
