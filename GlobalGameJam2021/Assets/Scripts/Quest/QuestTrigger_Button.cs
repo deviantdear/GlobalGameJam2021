022a2cc59838393cd30cfa9ad10dc7173489c883
vprime <myself@vincentprime.com>
2021-01-29 21:50:02 -0700

    Made Quest triggers for Quests, and Buttons

--- GlobalGameJam2021/Assets/Scripts/Quest/QuestTrigger_Button.cs
+++ GlobalGameJam2021/Assets/Scripts/Quest/QuestTrigger_Button.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class QuestTrigger_Button : MonoBehaviour
+public class QuestTrigger_Button : QuestTrigger
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button questTriggerButton = null;
+
+    private void Start()
     {
-        
+        questTriggerButton.onClick.AddListener(Trigger);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        questTriggerButton.onClick.RemoveListener(Trigger);
     }
 }
---
+++ GlobalGameJam2021/Assets/Scripts/Quest/QuestTrigger_Quest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger_Quest : QuestTrigger
+{
+    [SerializeField] private Quest watchedQuest = null;
+    [SerializeField] private QuestTriggerOption triggerOnState = QuestTriggerOption.SetCompleted;
+
+    private void Start()
+    {
+        switch (triggerOnState)
+        {
+            case QuestTriggerOption.SetActive:
+                watchedQuest.OnActive += Trigger;
+                break;
+            case QuestTriggerOption.SetAvailable:
+                watchedQuest.OnAvailable += Trigger;
+                break;
+            case QuestTriggerOption.SetCompleted:
+                watchedQuest.OnComplete += Trigger;
+                break;
+            case QuestTriggerOption.SetFailed:
+                watchedQuest.OnFailed += Trigger;
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        switch (triggerOnState)
+        {
+            case QuestTriggerOption.SetActive:
+                watchedQuest.OnActive -= Trigger;
+                break;
+            case QuestTriggerOption.SetAvailable:
+                watchedQuest.OnAvailable -= Trigger;
+                break;
+            case QuestTriggerOption.SetCompleted:
+                watchedQuest.OnComplete -= Trigger;
+                break;
+            case QuestTriggerOption.SetFailed:
+                watchedQuest.OnFailed -= Trigger;
+                break;
+        }
+    }
+}
