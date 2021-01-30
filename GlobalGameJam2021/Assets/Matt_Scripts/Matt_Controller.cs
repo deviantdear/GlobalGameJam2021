7611d45501661cee886dbdff0b332fe2493740c8
matthew3900 <mthomp28@asu.edu>
2021-01-30 13:49:12 -0700

    Google Dinosaur Game Completed
    My minigame is more or less complete, may need a bit of balancing/testing.
    
    There is dynamic difficulty (there are a preset list of obstacle prefabs ordered from least difficult to most, over time the least and most difficult prefabs that can be selected increase.
    
    There are end and win conditions (currently a min score threshold). Winning triggers a "cutscene" where the user arrives at some sort of win destination/finish line; the user cannot jump during this time.

---
+++ GlobalGameJam2021/Assets/Matt_Scripts/Matt_Controller.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Matt_Controller : MonoBehaviour
+{
+    private float basePlayerJumpVelocity = 12f;
+    [SerializeField] private float playerJumpVelocity = 12f;
+    [SerializeField] private float obstacleMoveVelcoity = 8f;
+    [SerializeField] private bool gameActive = true;
+    [SerializeField] private int score = 0;
+    [SerializeField] private int obstacleScoreValue = 10;
+    [SerializeField] private int scoreToWin = 500;
+
+    [SerializeField] private Text gameStatusText;
+    [SerializeField] private Text scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score >= scoreToWin)
+        {
+            StopPlayerJump();
+        }
+
+        if (gameActive)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void ChangeScore(int value)
+    {
+        score += value;
+        scoreText.text = "Score: " + score;
+    }
+
+
+    public bool GetGameActive()
+    {
+        return gameActive;
+    }
+
+    public float GetPlayerJumpVelocity()
+    {
+        return playerJumpVelocity;
+    }
+
+    public float GetObstacleMoveVelocity()
+    {
+        return obstacleMoveVelcoity;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public int GetScoreToWin()
+    {
+        return scoreToWin;
+    }
+
+    public int GetObstacleScoreValue()
+    {
+        return obstacleScoreValue;
+    }
+
+    public void StopPlayerJump()
+    {
+        playerJumpVelocity = 0f;
+    }
+
+    public void PlayerWins()
+    {
+        if (gameActive)
+        {
+            gameActive = false;
+            gameStatusText.text = "You Win!";
+            obstacleMoveVelcoity = 0f;
+        }
+    }
+
+
+    public void GameOver()
+    {
+        if (gameActive)
+        {
+            //Debug.Log("Game Over");
+            gameStatusText.text = "Game Over";
+            gameActive = false;
+            playerJumpVelocity = 0;
+            obstacleMoveVelcoity = 0f;
+        }
+    }
+
+
+}
---
+++ GlobalGameJam2021/Assets/Matt_Scripts/Matt_Spawner.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matt_Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstacles; // 0 = flying, 1 = ground
+
+    [SerializeField] int leastDifficultPrefab = 0;
+    [SerializeField] int mostDifficultPrefab = 3;
+
+    int tiersToIncreasePrefabDifficulty = 0;
+    [SerializeField] private GameObject winObject;
+    [SerializeField] private Matt_Controller gameController;
+
+    [SerializeField] private int tickCount = 0;
+    //[SerializeField] private int[] scoreThresholds = [0, 150, 300, 450, 600, 750, 1000];
+    //[SerializeField] private int[] tickSpeeds = [1200, 1100, 1000, 900, 800, 700, 600];
+    private int ticksBetweenSpawns = 1300;
+    private int minimumTicksBetweenSpawns = 700; // hardest speed
+
+    private bool spawnedMaxObjects = false; // if the spawner has created enough objects to reach the score needed to win
+
+    private int prefabsSpawnedTotal = 0;
+    private int prefabsSpawnedSinceChange = 0;
+    private int prefabsToIncreaseSpeed = 5;
+
+    private int obstaclesSpawnedTotal = 0;
+    private int obstaclesSpawnedSinceChange = 0;
+    private int objectsToIncreaseSpeed = 5;
+
+    bool maxObjects = false;
+    bool winSpawned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("Matt_Controller").GetComponent<Matt_Controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.GetGameActive())
+        {
+            if (gameController.GetGameActive() && winSpawned == false)
+            {
+                tickCount += 1;
+            }
+
+            if (tickCount > ticksBetweenSpawns && !maxObjects)
+            {
+                //int obstacleType = Random.Range(0, 5); // I want ground traps to be more common than flying ones, 0/1 = flying, 2/3/4 = ground
+
+                //if (obstacleType == 0 || obstacleType == 1)
+                //{
+                //    Instantiate(obstacles[0], new Vector3(this.gameObject.transform.position.x, flyingHeight, 0f), Quaternion.identity);
+                //}
+                //else if (obstacleType == 2 || obstacleType == 3 || obstacleType == 4)
+                //{
+                //    Instantiate(obstacles[1], new Vector3(this.gameObject.transform.position.x, groundHeight, 0f), Quaternion.identity);
+                //}
+
+                int obstacleType = Random.Range(leastDifficultPrefab, mostDifficultPrefab);
+
+                GameObject obstacleSpawned = obstacles[obstacleType];
+
+                Instantiate(obstacleSpawned, new Vector3(this.gameObject.transform.position.x, 0f, 0f), Quaternion.identity);
+
+                prefabsSpawnedTotal++;
+                prefabsSpawnedSinceChange++;
+
+                int obstaclesInPrefab = 0;
+
+                var children = obstacleSpawned.transform.root.GetComponentsInChildren<Transform>();
+                obstaclesInPrefab = children.Length - 1;
+
+                Debug.Log("Obstacles Spawned: " + obstaclesInPrefab);
+
+                //foreach (var child in children)
+                //{
+                //    obstaclesInPrefab++;
+                //}
+
+                obstaclesSpawnedTotal += obstaclesInPrefab;
+                obstaclesSpawnedSinceChange += obstaclesInPrefab;
+
+                maxObjects = CheckForMaxObjects();
+
+                tickCount = 0;
+
+                //if (obstaclesSpawnedSinceChange > objectsToIncreaseSpeed && ticksBetweenSpawns > minimumTicksBetweenSpawns)
+                if (prefabsSpawnedSinceChange > prefabsToIncreaseSpeed && ticksBetweenSpawns > minimumTicksBetweenSpawns)
+                {
+                    obstaclesSpawnedSinceChange = 0;
+                    prefabsSpawnedSinceChange = 0;
+                    ticksBetweenSpawns -= (int) ((float)ticksBetweenSpawns * .1f); // decrease time between spawns by 10% til minimum reached
+                    
+                    if (ticksBetweenSpawns < minimumTicksBetweenSpawns) { ticksBetweenSpawns = minimumTicksBetweenSpawns; }
+
+                    if (mostDifficultPrefab <= obstacles.Length)
+                    {
+                        tiersToIncreasePrefabDifficulty++;
+
+                        if (tiersToIncreasePrefabDifficulty % 2 == 0)
+                        {
+                            mostDifficultPrefab++;
+                        }
+
+                        if (tiersToIncreasePrefabDifficulty % 3 == 0)
+                        {
+                            if (leastDifficultPrefab < mostDifficultPrefab - 2)
+                            {
+                                leastDifficultPrefab++;
+                            }
+                        }
+                    }
+
+                    //Debug.Log("Decreased ticks between spawns to: " + ticksBetweenSpawns);
+
+                    //objectsToIncreaseSpeed = (int)(objectsToIncreaseSpeed + (float)objectsToIncreaseSpeed % .1f); // increase obstacles needed to reach next difficulty
+                }
+            }
+            else if (tickCount > ticksBetweenSpawns * 3 && maxObjects && winSpawned == false)
+            {
+                Instantiate(winObject, new Vector3(this.gameObject.transform.position.x, 0f, 0f), Quaternion.identity);
+                winSpawned = true;
+            }
+
+        }
+
+    }
+
+    bool CheckForMaxObjects()
+    {
+        if (obstaclesSpawnedTotal >= gameController.GetScoreToWin() / gameController.GetObstacleScoreValue())
+        {       
+            return true; // game has spawned enough obstacles for player to win
+        }
+
+        return false;
+
+    }
+
+}
