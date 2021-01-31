d946d38338f683a71c45a58027f20a11fa16f5e1
Anelius <47761414+Anelius@users.noreply.github.com>
2021-01-30 22:40:14 -0700

    Added trashman minigame

---
+++ GlobalGameJam2021/Assets/Scripts/Minigames/TrashMan/CanLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanLogic : MonoBehaviour {
+    public GameObject Trash;
+    public int TotalTrash;
+    public bool Victory = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (TotalTrash <= 0) {
+            TotalTrash = (int)Random.Range(9, 15);
+        }
+
+        for (int i = 0; i < TotalTrash; i++) {
+            Vector2 pos = new Vector2(transform.position.x+Random.Range(1, 16.5f),
+                transform.position.y-Random.Range(1, 9));
+
+            GameObject tmp = Instantiate(Trash, pos, Quaternion.identity);
+            tmp.transform.SetParent(this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (TotalTrash <= 0 && !Victory) {
+            Victory = true;
+        }
+    }
+}
