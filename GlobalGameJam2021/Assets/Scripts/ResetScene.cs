0e72a30e657ad2c629f2bd334c22fa532957f660
vprime <myself@vincentprime.com>
2021-01-31 16:10:20 -0700

    Added scene cleanup script after completing a quest.

--- GlobalGameJam2021/Assets/Scripts/ResetScene.cs
+++ GlobalGameJam2021/Assets/Scripts/ResetScene.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ResetScene : MonoBehaviour
 {
+    private List<GameObject> initalState = null;
+
+    [SerializeField] private Quest quest = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initalState = new List<GameObject>(SceneManager.GetActiveScene().GetRootGameObjects());
+        quest.OnActive += ResetRoot;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetRoot(bool activeState)
     {
-        
+        var currentState = new List<GameObject>(SceneManager.GetActiveScene().GetRootGameObjects());
+        foreach (var item in currentState)
+        {
+            if (!initalState.Contains(item))
+                Destroy(item);
+        }
     }
 }
